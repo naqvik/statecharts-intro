67fbc0053492914d13ba31a70dbc71bcf5c46b4b
Karim Naqvi <naqvik@uregina.ca>
2024-03-04 12:46:50 -0600

    Add fake on/off button, print its state

--- main.c
+++ main.c
@@ -20,9 +20,20 @@
 typedef enum  {OFF, SLOW, MEDIUM, FAST} CurrState;
 CurrState currState = OFF;
 
+bool onOffButton() {
+    static const bool sequence[] = {1,0,0,0,0,0,0,};
+    uint32_t sequence_size = sizeof(sequence)/sizeof(sequence[0]);
+    static uint32_t idx = 0;
+
+    bool retval = sequence[idx];
+    idx = (idx+1) & sequence_size;
+    return retval;
+}
+
 int main() {
     while (1) {
         printf("currState: %d\n", currState);
+        printf("on-off state: %d\n", onOffButton());
         sleep(1);  // sleep 1 second
     }
 }
